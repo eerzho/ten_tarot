1a2fdadd4a3aa4c34f98e2128c02446a50b42755
zhanbolat <z.yerkinbay@slotegrator.space>
2024-07-29 23:40:23 +0200

    refactoring command

--- internal/handler/telegram/v1/command.go
+++ internal/handler/telegram/v1/command.go
@@ -1,9 +1,6 @@
 package v1
 
 import (
-	"context"
-	"strconv"
-
 	"github.com/eerzho/ten_tarot/pkg/logger"
 	"gopkg.in/telebot.v3"
 )
@@ -26,21 +23,13 @@ func newCommand(bot *telebot.Bot, tgUserService tgUserService) *command {
 
 func (c *command) start(ctx telebot.Context) error {
 	const op = "handler.telegram.v1.command.start"
+	logger.Debug(op, logger.Any("RID", ctx.Get(RID)))
 
-	_, err := c.tgUserService.GetOrCreateByChatIDUsername(
-		context.Background(),
-		strconv.Itoa(int(ctx.Sender().ID)),
-		ctx.Sender().Username,
-	)
-	if err != nil {
-		logger.OPError(op, err)
-	}
-
-	if _, err = ctx.Bot().Send(
-		ctx.Sender(),
-		"Я ваш личный Таролог и готов помочь вам получить ответы на любые вопросы. "+
-			"Просто отправьте свой вопрос, и я сделаю расклад на Таро специально для вас.\n\n"+
-			"✨Будьте готовы узнать, что приготовила для вас судьба✨"); err != nil {
+	if err := ctx.Send(
+		"Я ваш личный Таролог и готов помочь вам получить ответы на любые вопросы. " +
+			"Просто отправьте свой вопрос, и я сделаю расклад на Таро специально для вас.\n\n" +
+			"✨Будьте готовы узнать, что приготовила для вас судьба✨",
+	); err != nil {
 		logger.OPError(op, err)
 		return err
 	}
